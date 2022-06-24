ef888eaf4966c90a49221750c8b7957cc8eb0b52
subhash505 <mr.subhash505@gmail.com>
2022-06-24 16:34:29 +0530

    changes done

---
+++ Q12.py
@@ -0,0 +1,8 @@
+list1 = [5,10,15,20,25,50,20]
+x=20
+if x in list1:
+    y=list1.index(x)
+    print("yes it is there in the list at this position :",y)
+    list1.remove(x)
+    list1.insert(y,200)
+print("now the updated list as per question: \n",list1)
