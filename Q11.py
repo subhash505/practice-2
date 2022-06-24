23cff81cbf151394fcf1a6ee94bd547fda2483a8
subhash505 <mr.subhash505@gmail.com>
2022-06-24 14:59:14 +0530

    chages done

---
+++ Q11.py
@@ -0,0 +1,9 @@
+list1 = ["a","b",["c",["d","e",["f", "g"],"k"],"l"],"m","n"]
+sub_list=["h","i","j"]
+list1[2][1][2].extend(sub_list)
+print(list1)
+print("Given List:\n")
+print("""List1=["a","b",["c",["d","e",["f", "g"],"k"],"l"],"m","n"]\n""")
+print("""Sub List to be added = ["h", "i", "j"]""")
+print("Expected output:\n",list1)
+
